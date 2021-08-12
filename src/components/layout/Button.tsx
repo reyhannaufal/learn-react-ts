792a7c48a56f8b45a1d3e2a5b7f11f1fcd208576
reyhannaufal <reyhannaufal21@gmail.com>
2021-08-13 06:09:45 +0800

    feat: add button components

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -1,7 +1,19 @@
 type ButtonProps = {
   text: string;
+  onClick?: () => void;
+  className?: string;
 };
 
-export default function Button({ text }: ButtonProps) {
-  return <div>aa</div>;
+export default function Button({ text, onClick, className }: ButtonProps) {
+  return (
+    <>
+      <button
+        type="button"
+        onClick={onClick}
+        className={`inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${className}`}
+      >
+        {text}
+      </button>
+    </>
+  );
 }
